6930a4a655649b28e43de436e9e998e3d97a89a0
Adrian Suarez <asz6@3ds.com>
2024-09-22 15:25:03 -0400

    Add generated files

---
+++ internal/controller/zz_setup.go
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controller
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/crossplane/upjet/pkg/controller"
+
+	database "github.com/nuodb/provider-nuodbaas/internal/controller/database/database"
+	project "github.com/nuodb/provider-nuodbaas/internal/controller/project/project"
+	providerconfig "github.com/nuodb/provider-nuodbaas/internal/controller/providerconfig"
+)
+
+// Setup creates all controllers with the supplied logger and adds them to
+// the supplied manager.
+func Setup(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		database.Setup,
+		project.Setup,
+		providerconfig.Setup,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
